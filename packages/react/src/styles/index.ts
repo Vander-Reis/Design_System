aff87b05250d748f91fd043d24bbc52893a22d31
Vander-Reis <vanderreis2017@outlook.com>
2022-12-13 16:23:56 -0300

    correção

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from "@ignite-ui/tokens";
+} from "@vander-ignite-ui/tokens";
 import { createStitches, defaultThemeMap } from "@stitches/react";
 
 export const {
