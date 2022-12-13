6b8ba2a42c9f6752e132612dca879a385880f746
Vander-Reis <vanderreis2017@outlook.com>
2022-12-13 11:19:22 -0300

    Addon de acessibilidade

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: "https://github.com/vander-reis.png",
     alt: "Vander Reis",
   },
+  argTypes: {
+    src: {
+      control: {
+        type: "text",
+      },
+    },
+  },
 } as Meta<AvatarProps>;
 
 export const Primary: StoryObj<AvatarProps> = {};
